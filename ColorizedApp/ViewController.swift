6badfaddcae6392d63be4b22dd03a005198a2ea7
Mikhail German <mikhail.german@icloud.com>
2023-02-08 01:40:19 +0500

    created IBOutlets and IBActions added func viewColorChange()

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,12 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+ 
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 15
+        redLabel.text = redSlider.minimumValue.formatted()
+        greenLabel.text = greenSlider.minimumValue.formatted()
+        blueLabel.text = blueSlider.minimumValue.formatted()
+        
     }
+    
+    // MARK: - SliderActions
+    @IBAction func redsliderAction() {
+        redSlider.value = Float(round(100 * redSlider.value) / 100)
+        redLabel.text = redSlider.value.formatted()
+        viewColorChange()
+        
+    }
+    
+  
+    @IBAction func greenSliderAction() {
+        greenSlider.value = Float(round(100 * greenSlider.value) / 100)
+        greenLabel.text = greenSlider.value.formatted()
+        viewColorChange()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueSlider.value = Float(round(100 * blueSlider.value) / 100)
+        blueLabel.text = blueSlider.value.formatted()
+        viewColorChange()
+    }
+    
+   private func viewColorChange() {
+        colorView.backgroundColor = UIColor (
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: (1))
+    }
+    
+   
 
 
 }
